40641e3f443ae631cce637d66f188c1b699db476
rosaainz <rosagsainzz@gmail.com>
2025-10-23 15:20:11 -0600

    feat: convert celsius to kelvin and farenheit and show in dom

--- app.js
+++ app.js
@@ -9,4 +9,16 @@ document.getElementById('formTemp').addEventListener("submit", function(event) {
         result.style.color  = "red";
         return;
     }
+
+    const kelvin = celsius + 273.15;
+    const farenheit = (celsius * 9/5) + 32;
+
+    result.style.color = "black";
+    result.innerHTML = `
+        <p>${celsius.toFixed(2)} °C son:</p>
+        <ul>
+            <li>${kelvin.toFixed(2)} K</li>
+            <li>${farenheit.toFixed(2)} °F</li>
+        </ul>
+    `;
 });
